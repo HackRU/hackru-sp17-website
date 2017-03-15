a2f7157201985bad8500b2bad7bd37ddf02759bf
dchen <chendavid96@gmail.com>
2017-03-15 00:01:17 -0400

    committed and pushed on Wed, Mar 15, 2017 12:01:17 AM

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 	smoothScrollEnable();
+	toggleMapScrollOnClickAndLeave();
+
+	var heroBottom = $('#hero').height();
+	$(window).on('scroll',function(){
+
+	    // we round here to reduce a little workload
+	    var stop = Math.round($(window).scrollTop());
+
+	    if (stop > heroBottom) {
+	        $('#nav-secondary').fadeIn();
+	    } else {
+	        $('#nav-secondary').fadeOut();
+	    }
+
+	});	
+
 });
 
 
@@ -9,4 +25,14 @@ function smoothScrollEnable() {
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 	});
 	return;
+}
+
+function toggleMapScrollOnClickAndLeave() {
+	$('#about-location-map').click(function () {
+		$('#about-location-map iframe').css("pointer-events", "auto");
+	});
+
+	$( "#about-location-map" ).mouseleave(function() {
+		$('#about-location-map iframe').css("pointer-events", "none"); 
+	});
 }
