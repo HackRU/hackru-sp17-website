8b220ce650443fef2c7e486bf4d8937881597be8
dchen <chendavid96@gmail.com>
2017-03-24 17:27:34 -0400

    committed and pushed on Fri, Mar 24, 2017  5:27:34 PM

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,46 @@
-$( document ).ready(function() {
-	smoothScrollEnable();
-	toggleMapScrollOnClickAndLeave();
-
-	var heroBottom = $('#hero').height();
-	$(window).on('scroll',function(){
-	    // we round here to reduce a little workload
-	    var stop = Math.round($(window).scrollTop());
-
-	    if (stop > heroBottom) {
-	    	$('#nav-secondary').fadeIn();
-	    } else {
-	    	$('#nav-secondary').fadeOut();
-	    }
-	});
-
-	// on resume file upload, change the notification to say what file was uploaded
-
-	$('#resume-upload-input').on("change", function(){
-		changeFileUploadPreview();
-	});
+// check if current position is past halfpoint of hero
+// if so, we make the secondary navbar appear on top
+$(window).on('scroll', function() {
+	console.log("scrolling...");
+	var heroBottomHalf = $('#hero').height() / 2;
+    var stop = Math.round($(window).scrollTop());
+    if (stop > heroBottomHalf) {
+    	$('#nav-secondary').fadeIn();
+    } else {
+    	$('#nav-secondary').fadeOut();
+    }
 });
 
 
+$(document).ready(function() {
+	
+	var pageLocation = window.location.pathname; // used to run certain JS on specific pages
+	
+	if (pageLocation == '/' ) { // run on home page (landing page)
+		smoothScrollEnable();
+		toggleMapScrollOnClickAndLeave();
+	}
+	else {
+		loadExternalHTMLTemplates();
+		
+		// on resume file upload, change the notification to say what file was uploaded
+		$('#resume-upload-input').on("change", function(){
+			changeFileUploadPreview();
+		});
+	}
+
+	
+});
+
+
+function loadExternalHTMLTemplates() {
+	// templating.
+	$(".subpage-nav").load("../templates/subpage-nav.html"); // load nav bar for all subpages
+	return;
+}
+
 function smoothScrollEnable() {
+	// helps smoothly transition to anchor points on the landing page.
 	$('a[href*=\\#]').on('click', function(event){     
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
@@ -31,6 +49,7 @@ function smoothScrollEnable() {
 }
 
 function toggleMapScrollOnClickAndLeave() {
+	// for use on the google map on the landing page.
 	$('#about-location-map').click(function () {
 		$('#about-location-map iframe').css("pointer-events", "auto");
 	});
@@ -41,7 +60,7 @@ function toggleMapScrollOnClickAndLeave() {
 }
 
 function changeFileUploadPreview(){
-
+	// for use in resume uploading on the registration and account management pages.
     var fileName = $("#resume-upload-input").val().replace(/\\/g, '/').replace(/.*\//, '');
     $("#file-selector-notif").text(fileName);
     console.log(fileName);
